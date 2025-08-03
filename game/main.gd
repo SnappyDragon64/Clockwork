12f99e95605aacd887afa84ca813785aa719ba27
SnappyDragon64 <pg61151@gmail.com>
2025-08-03 22:16:53 +0530

    Main menu on load

--- game/main.gd
+++ game/main.gd
@@ -5,4 +5,4 @@ func _ready():
 	SceneManager.load_scene(Scenes.WORLD_MUSIC_PLAYER)
 	await SceneManager.load_scene(Scenes.TRANSITION_DEFAULT).completed
 	TransitionManager.set_current_transition(Transitions.DEFAULT)
-	SceneSetManager.change_set(SceneSets.LEVEL1)
+	SceneSetManager.change_set(SceneSets.MENU_MAIN_MENU)
